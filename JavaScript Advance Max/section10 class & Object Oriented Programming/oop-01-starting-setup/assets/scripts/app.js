ef4beac11c18f1bf0b22fee019f2383cd68c7cd4
Has9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-01-05 04:38:49 +0300

    working with multi classes

--- JavaScript Advance Max/section10 class & Object Oriented Programming/oop-01-starting-setup/assets/scripts/app.js
+++ JavaScript Advance Max/section10 class & Object Oriented Programming/oop-01-starting-setup/assets/scripts/app.js
@@ -13,61 +13,56 @@ class Product {
   }
 }
 
-console.log(new Product());
-const productList = {
-  products: [
-    // this return an Object
-    new Product( 
+class ProductItem{
+    constructor(product){
+        this.product = product;
+    }
+    render(){
+        const prodEl = document.createElement("li");
+        prodEl.className = "product-item";
+        prodEl.innerHTML = `
+              <div>
+                  <img src = "${this.Product.imageUrl}" alt=${this.Product.title} >
+                  <div class = product-item__content>
+                      <h2>${this.Product.title}</h2>
+                      <h3>${this.Product.price} </h3>
+                      <p>${this.Product.description}</p>
+                      <button>Add to Cart</button>
+                  </div>
+              </div>
+              `;
+        return prodEl ; 
+    }
+}
+
+class ProductList {
+  products = [
+    new Product(
       "A pillow",
       "https://contentgrid.thdstatic.com/hdus/en_US/DTCCOMNEW/fetch/NexGen/ContentPage/2019-CARPET-Features-softness.jpg",
       "A soft pillow!",
       19.99
     ),
-    new Product( 
-        "A Carpet",
-        "https://unitedpillow.com/images/companies/2/pillow-thumb.jpg?1564370699038",
-        "A soft Carpet!",
+    new Product(
+      "A Carpet",
+      "https://unitedpillow.com/images/companies/2/pillow-thumb.jpg?1564370699038",
+      "A soft Carpet!",
       89.99
     ),
-   /*
-    {
-      title: "A pillow",
-      imageUrl:
-        "https://contentgrid.thdstatic.com/hdus/en_US/DTCCOMNEW/fetch/NexGen/ContentPage/2019-CARPET-Features-softness.jpg",
-      price: 19.99,
-      description: "A soft pillow!",
-    },
-    
-    {
-      title: "A Carpet",
-      imageUrl:
-        "https://unitedpillow.com/images/companies/2/pillow-thumb.jpg?1564370699038",
-      price: 89.99,
-      description: "A soft Carpet!",
-    },
-    */
-  ],
+  ];
+  constructor() {}
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-        <div>
-            <img src = "${prod.imageUrl}" alt=${prod.title} >
-            <div class = product-item__content>
-                <h2>${prod.title}</h2>
-                <h3>${prod.price} </h3>
-                <p>${prod.description}</p>
-                <button>Add to Cart</button>
-            </div>
-        </div>
-        `;
+      const product = new ProductItem(prod)
+      const prodList = ProductItem.render(); 
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
-  },
-};
+  }
+}
+console.log(new Product());
+const productList = new ProductList();
 productList.render();
