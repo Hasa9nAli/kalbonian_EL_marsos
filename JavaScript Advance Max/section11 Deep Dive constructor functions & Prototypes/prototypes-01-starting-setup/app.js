5e676f1f68b2c5ea7a1eab225bf1ae6361c3cc67
Has9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-01-11 09:52:06 +0300

    end the section 11

--- JavaScript Advance Max/section11 Deep Dive constructor functions & Prototypes/prototypes-01-starting-setup/app.js
+++ JavaScript Advance Max/section11 Deep Dive constructor functions & Prototypes/prototypes-01-starting-setup/app.js
@@ -1,26 +1,53 @@
-// class Person {
-//   name = "Max";
-//   constructor() {
-//     this.age = 30;
-//   }
-//   greet() {
-//     console.log(
-//       "Hi, I am " + this.name + "and i am " + this.age + " years old"
-//     );
-//   }
-// }
-
-function Person (){
-    this.age = 30 ; 
-    this.name = 'Max' ; 
-    this.greet = ()=>{
-        console.log(
-                  "Hi, I am " + this.name + "and i am " + this.age + " years old"
-                )
-    };
+class AgedPerson{
+    printAge(){
+        console.log(this.age)
+    }
 }
+
+class Person extends AgedPerson{
+  name = "Max";
+  constructor() {
+    super()
+    this.age = 30;
+    this.greet()
+  }
+  greet() {
+    console.log(
+      "Hi, I am " + this.name + "and i am " + this.age + " years old"
+    );
+  }
+}
+
+// function Person (){
+//     this.age = 30 ; 
+//     this.name = 'Max' ; 
+//     this.greet = ()=>{
+//         console.log(
+//                   "Hi, I am " + this.name + "and i am " + this.age + " years old"
+//                 )
+//     };
+// }
 /* _________________that Error________________
 const person = Person(); 
 */
 const person = new Person();
 person.greet()
+// person.printAge()
+console.log(person.__proto__)
+console.log(String.prototype)
+console.log(person.__proto__)
+Person.prototype.printValue =     console.log("hello from prototype"); 
+
+Person.printValue
+ const p = new person.__proto__.constructor()
+Person.describe = function (){
+  console.log("Creating person...")
+}
+Person.describe()
+console.log(p)
+const age = new AgedPerson()
+// age.greet()
+
+const v1 = new Person()
+const v2 = new Person(); 
+console.log(v1.__proto__ === v2.__proto__)
