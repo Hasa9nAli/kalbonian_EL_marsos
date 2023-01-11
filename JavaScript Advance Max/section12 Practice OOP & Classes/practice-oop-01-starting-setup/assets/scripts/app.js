82b5dfa421748c36a936dba563771a74c5b755ba
Has9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-01-11 10:17:23 +0300

    Start work with section 12

---
+++ JavaScript Advance Max/section12 Practice OOP & Classes/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,20 @@
+class Tooltip{
+
+}
+class ProjectItem{
+
+}
+class ProjectList{
+    constructor(type){
+        const prjItems = document.querySelectorAll(`#${type}-projects li`)
+        console.log(prjItems)
+    }
+}
+class App{
+    static init(){
+        const activeProjectList = new ProjectList('active')
+        const finishProjectList = new ProjectList('finished')
+    }
+}
+
+App.init()
