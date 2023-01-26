a43d126687103e5e87cdf06490723b00508a5ebf
Has9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-01-26 17:34:41 +0300

    end section 14

--- JavaScript Advance Max/section14 working with event/events/assets/scripts/events.js
+++ JavaScript Advance Max/section14 working with event/events/assets/scripts/events.js
@@ -50,27 +50,33 @@ form.addEventListener("submit", (event)=>{
     console.log(event) ;
 })
 
-let curElementNumber = 0;
+// let curElementNumber = 0;
  
-function scrollHandler() {
-    const distanceToBottom = document.body.getBoundingClientRect().bottom;
+// function scrollHandler() {
+//     const distanceToBottom = document.body.getBoundingClientRect().bottom;
  
-    if (distanceToBottom < document.documentElement.clientHeight + 150) {
-        const newDataElement = document.createElement('div');
-        curElementNumber++;
-        newDataElement.innerHTML = `<p>Element ${curElementNumber}</p>`;
-        document.body.append(newDataElement);
-    }
-}
+//     if (distanceToBottom < document.documentElement.clientHeight + 150) {
+//         const newDataElement = document.createElement('div');
+//         curElementNumber++;
+//         newDataElement.innerHTML = `<p>Element ${curElementNumber}</p>`;
+//         document.body.append(newDataElement);
+//     }
+// }
+// window.addEventListener('scroll', scrollHandler);
  
 div.addEventListener("click", (event)=>{
     event.stopPropagation();
     console.log("DIV CLICKED")
     console.log(event)});
-window.addEventListener('scroll', scrollHandler);
+
+const submitBtn = document.querySelector(".submit-btn")
+
+submitBtn = submitBtn.addEventListener("click",()=>{
 
 
 
+    
+})
 
 // button.addEventListener("contextmenu", buttonClickHandler)
 // anotherButton.addEventListener("click", eventHappenWhenClickedToAnotherButton )
